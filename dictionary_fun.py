697de4c129022dd30b160973642b5e9a4a54a8da
Jose Quijada <joquijada2010@gmail.com>
2021-11-03 09:41:17 -0400

    feat(code): save

--- dictionary_fun.py
+++ dictionary_fun.py
@@ -6,10 +6,14 @@ import random
 
 my_dict = {'shit': 'crap'}
 
-# Below raises KeyError exception because gthe key is not in the dictionary. I guess Python designers
+# Below raises KeyError exception because the key is not in the dictionary. I guess Python designers
 # preferred an error be raised early on to avoid the dreaded NPE type exception raised in Java which happens when you
 # would go to dereference a property of a 'null' object
 # print(my_dict['foo'])
+
+# Below is one way to check if a dictionary contains a key, https://stackoverflow.com/questions/1602934/check-if-a-given-key-already-exists-in-a-dictionary
+print('foo' in my_dict)
+print('shit' in my_dict)
 
 # Below does not work like Groovy's default Map.get('key', 'default val'), because it will not
 # insert the default, just returns it...
@@ -19,7 +23,7 @@ print(my_dict.get('foo', 'bar'))
 print(my_dict.setdefault('bull', 'dong'))
 
 print(my_dict)
-+
+
 # Accessing dictionaries, can't use index, below throws KeyError
 # print(my_dict[0])
 
